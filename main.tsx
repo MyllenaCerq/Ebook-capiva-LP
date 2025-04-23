4a3666e2171be0431be9d15e100ba52beeed6e16
Myllena Cerqueira <114107187+MyllenaCerq@users.noreply.github.com>
2025-04-23 17:02:49 -0300

    feat: versão funcional conectada ao n8n e pronta para deploy

---
+++ main.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import App from './frontend/src/App.tsx'
+import './frontend/src/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+createRoot(document.getElementById("root")!).render(<App />);
